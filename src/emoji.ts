eb9ec7473ac970d07cb0e6b810b9df1dd30ba6f7
Mahmoud Galal <ssss.aaa2002@gmail.com>
2024-03-04 20:13:47 +0200

    prepare bundle

--- src/emoji.ts
+++ src/emoji.ts
@@ -1,11 +1,11 @@
 export function getEmojiForNumber(number: number): string {
   if (number <= 25) {
-      return '📛';
+    return '📛'
   } else if (number <= 50) {
-      return '😐🔎'; // Emoji for range 2
+    return '😐🔎' // Emoji for range 2
   } else if (number <= 75) {
-      return '😊⚙️'; // Emoji for range 3
+    return '😊⚙️' // Emoji for range 3
   } else {
-      return '🟢🚀'; // Emoji for range 4
+    return '🟢🚀' // Emoji for range 4
   }
 }
